125bb61fc75dc9c1339e13bde1d79072fc3a111b
saposki <saposki@gmail.com>
2016-12-07 18:34:35 -0800

    params completed

--- module4/app.js
+++ module4/app.js
@@ -3,6 +3,10 @@ var app = express();
 
 var PORT = 8080;
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
+app.get('/team/:name', function(req, res){
+  res.setHeader('Content-Type', 'text/plain');
+  res.send('You picked' + req.params.name);
+});
 
 var server = app.listen(PORT);
