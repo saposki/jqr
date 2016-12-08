4082047dd9317bec1148c098efea5f89a308f913
saposki <saposki@gmail.com>
2016-12-07 20:10:00 -0800

    eod

--- module4/app.js
+++ module4/app.js
@@ -6,7 +6,7 @@ var PORT = 8080;
 // app.use(express.static(__dirname + '/public'));
 app.get('/team/:name', function(req, res){
   res.setHeader('Content-Type', 'text/plain');
-  res.send('You picked' + req.params.name);
+  res.send('You picked ' + req.params.name);
 });
 
 var server = app.listen(PORT);
